e4fe6304ffad2ba045423fff7362655a28c7f18c
SakshiBerad <beradsakshi104@gmail.com>
2025-05-20 09:38:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+import TextEditor from "./TextEditor";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/docs/${uuidV4()}`} />} />
+        <Route path="/docs/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
